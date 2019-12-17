31ae19f27f8dbb5556007c8227c907f15b160f0f
Doug Barry <doug@gtko.co.uk>
2019-12-17 22:53:12 +0000

    dougbarry: Added OLED code to wifi, added wifi event scaffold

--- wifi.h
+++ wifi.h
@@ -29,6 +29,60 @@
 // AP mode password
 //const char WiFiAPPSK[] = "";
 
+void WiFiEvent(WiFiEvent_t event)
+{
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch (event)
+  {
+    case SYSTEM_EVENT_WIFI_READY:               /**< ESP32 WiFi ready */
+      break;
+    case SYSTEM_EVENT_SCAN_DONE:                /**< ESP32 finish scanning AP */
+      break;
+
+    case SYSTEM_EVENT_STA_START:                /**< ESP32 station start */
+      break;
+    case SYSTEM_EVENT_STA_STOP:                 /**< ESP32 station stop */
+      break;
+
+    case SYSTEM_EVENT_STA_CONNECTED:            /**< ESP32 station connected to AP */
+      break;
+
+    case SYSTEM_EVENT_STA_DISCONNECTED:         /**< ESP32 station disconnected from AP */
+      break;
+
+    case SYSTEM_EVENT_STA_AUTHMODE_CHANGE:      /**< the auth mode of AP connected by ESP32 station changed */
+      break;
+
+    case SYSTEM_EVENT_STA_GOT_IP:               /**< ESP32 station got IP from connected AP */
+    case SYSTEM_EVENT_STA_LOST_IP:              /**< ESP32 station lost IP and the IP is reset to 0 */
+      break;
+
+    case SYSTEM_EVENT_STA_WPS_ER_SUCCESS:       /**< ESP32 station wps succeeds in enrollee mode */
+    case SYSTEM_EVENT_STA_WPS_ER_FAILED:        /**< ESP32 station wps fails in enrollee mode */
+    case SYSTEM_EVENT_STA_WPS_ER_TIMEOUT:       /**< ESP32 station wps timeout in enrollee mode */
+    case SYSTEM_EVENT_STA_WPS_ER_PIN:           /**< ESP32 station wps pin code in enrollee mode */
+      break;
+
+    case SYSTEM_EVENT_AP_START:                 /**< ESP32 soft-AP start */
+    case SYSTEM_EVENT_AP_STOP:                  /**< ESP32 soft-AP stop */
+    case SYSTEM_EVENT_AP_STACONNECTED:          /**< a station connected to ESP32 soft-AP */
+    case SYSTEM_EVENT_AP_STADISCONNECTED:       /**< a station disconnected from ESP32 soft-AP */
+    case SYSTEM_EVENT_AP_PROBEREQRECVED:        /**< Receive probe request packet in soft-AP interface */
+    case SYSTEM_EVENT_AP_STA_GOT_IP6:           /**< ESP32 station or ap interface v6IP addr is preferred */
+    case SYSTEM_EVENT_AP_STAIPASSIGNED:
+      break;
+
+    case SYSTEM_EVENT_ETH_START:                /**< ESP32 ethernet start */
+    case SYSTEM_EVENT_ETH_STOP:                 /**< ESP32 ethernet stop */
+    case SYSTEM_EVENT_ETH_CONNECTED:            /**< ESP32 ethernet phy link up */
+    case SYSTEM_EVENT_ETH_DISCONNECTED:         /**< ESP32 ethernet phy link down */
+    case SYSTEM_EVENT_ETH_GOT_IP:               /**< ESP32 ethernet got IP from connected AP */
+    case SYSTEM_EVENT_MAX:
+      break;
+  }
+}
+
+
 void setupWifi() {
   //  // Set Hostname.
   //  String hostname(HOSTNAME);
@@ -60,13 +114,22 @@ void setupWifi() {
   //  else
   //  {
   WiFi.mode(WIFI_STA);
-  Serial.println("Starting WiFi...");
-  Serial.print("WiFi MAC Address: ");
-  Serial.println(WiFi.macAddress());
-  Serial.printf("Connecting to %s\n", ssid);
+  WiFi.onEvent(WiFiEvent);
+
+  String message;
+  message = "WiFi: Starting \nWiFi: MAC Address: \n" + WiFi.macAddress() + "\n WiFi: Connecting to " + ssid;
+
+  Heltec.display->clear();
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->drawStringMaxWidth(0, 0, 128, message);
+  //Heltec.display->drawString(0, 0, message);
+
+  Serial.println(message);
+  Heltec.display->display();
+
   if (String(WiFi.SSID()) != String(ssid)) {
     WiFi.begin(ssid, password);
   }
-  
+
   //  }
 }
